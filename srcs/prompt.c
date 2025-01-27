790baf22158791cd1eda7f480f59ddba1bd64557
Alain <alain.sn.66@gmail.com>
2025-01-27 15:47:07 +0100

    rm prompt color

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsebasti <rsebasti@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 21:56:27 by asene             #+#    #+#             */
-/*   Updated: 2025/01/27 11:17:39 by rsebasti         ###   ########.fr       */
+/*   Updated: 2025/01/27 14:53:00 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ char	*get_prompt(t_vars *vars)
 	char	*prompt;
 
 	i = 0;
-	prompt_parts[i++] = "\001\033[1;32m\001minishell\001\033[0m\001:";
-	prompt_parts[i++] = "\001\033[1;34m\001";
+	prompt_parts[i++] = "minishell:";
 	pwd = getcwd(NULL, 0);
 	home = getenv_value(vars, "HOME");
 	if (pwd && home && ft_strncmp(home, pwd, ft_strlen(home)) == 0)
@@ -33,7 +32,7 @@ char	*get_prompt(t_vars *vars)
 	else if (pwd)
 		prompt_parts[i++] = pwd;
 	free(home);
-	prompt_parts[i++] = "\002\033[0m\002 > ";
+	prompt_parts[i++] = " > ";
 	prompt = ft_strnjoin(prompt_parts, i, "");
 	free(pwd);
 	return (prompt);
