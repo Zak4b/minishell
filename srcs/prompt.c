c00033e3376b1f875d5d7263350ef54d9dfd3752
Alain <zakab.66@gmail.com>
2025-02-18 13:29:11 +0100

    prompt color

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 21:56:27 by asene             #+#    #+#             */
-/*   Updated: 2025/01/27 14:53:00 by asene            ###   ########.fr       */
+/*   Updated: 2025/02/18 13:28:35 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*get_prompt(t_vars *vars)
 	char	*prompt;
 
 	i = 0;
-	prompt_parts[i++] = "minishell:";
+	prompt_parts[i++] = "\001\e[1;32m\002minishell:\001\e[1;34m\002";
 	pwd = getcwd(NULL, 0);
 	home = getenv_value(vars, "HOME");
 	if (pwd && home && ft_strncmp(home, pwd, ft_strlen(home)) == 0)
@@ -32,7 +32,7 @@ char	*get_prompt(t_vars *vars)
 	else if (pwd)
 		prompt_parts[i++] = pwd;
 	free(home);
-	prompt_parts[i++] = " > ";
+	prompt_parts[i++] = " > \001\e[0m\002";
 	prompt = ft_strnjoin(prompt_parts, i, "");
 	free(pwd);
 	return (prompt);
